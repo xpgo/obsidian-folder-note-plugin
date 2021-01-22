9f50dcaff76381176aaf2dc4dfaf788d69556569
xpgo <xiaopan@lnm.imech.ac.cn>
2021-01-22 10:20:33 +0800

    add settings for note name and initial contents

--- main.ts
+++ main.ts
@@ -2,18 +2,20 @@ import { fstat } from 'fs';
 import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, getLinkpath } from 'obsidian';
 
 interface FolderNotePluginSettings {
-	mySetting: string;
+	folderNoteName: string;
+	folderNoteStrInit: string;
 }
 
 const DEFAULT_SETTINGS: FolderNotePluginSettings = {
-	mySetting: 'default'
+	folderNoteName: '_about_',
+	folderNoteStrInit: '# About the Folder\n'
 }
 
 export default class FolderNotePlugin extends Plugin {
 	settings: FolderNotePluginSettings;
 
 	async onload() {
-		// console.log('loading Folder Note plugin');
+		console.log('Loading Folder Note plugin');
 
 		await this.loadSettings();
 
@@ -21,35 +23,34 @@ export default class FolderNotePlugin extends Plugin {
 		// 	new Notice('Auto generate brief description for folder note!');
 		// });
 
+		this.addSettingTab(new FolderNoteSettingTab(this.app, this));
+
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) =>  {
 			// get the folder path
 			var folderPath = '';
 			const elemTarget = (evt.target as Element);
-			if (elemTarget.className == 'nav-folder-title') {
-				folderPath = elemTarget.attributes.getNamedItem('data-path').textContent;
+			var className = elemTarget.className.toString();
+			var folderElem = elemTarget;
+			if (className.contains('nav-folder-title-content')) {
+				folderElem = elemTarget.parentElement;
+				folderPath = folderElem.attributes.getNamedItem('data-path').textContent;
 			}
-			else if (elemTarget.className == 'nav-folder-title-content') {
-				const elemTitle = elemTarget.parentElement;
-				folderPath = elemTitle.attributes.getNamedItem('data-path').textContent;
+			else if (className == 'nav-folder-title') {
+				folderPath = elemTarget.attributes.getNamedItem('data-path').textContent;
 			}
 
 			// open the infor note
 			if (folderPath.length > 0) {
-				if (evt.ctrlKey) {
-					this.createFoldNote(folderPath);
-				} else {
-					this.openFoldNote(folderPath);
-				}
-				// console.log('note link:', evt.ctrlKey);
+				this.openFoldNote(folderElem, folderPath, evt.ctrlKey);
 			}
 		});
 
-		// interval func
+		// interval func - keep it, maybe used for auto generation
 		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-		// console.log('unloading Folder Note plugin');
+		console.log('Unloading Folder Note plugin');
 	}
 
 	async loadSettings() {
@@ -60,26 +61,83 @@ export default class FolderNotePlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 
-	async openFoldNote(folderPath: string) {
-		const noteName = '_about_'
+	async openFoldNote(folderElem: Element, folderPath: string, doCreate: boolean) {
+		// check note file
+		const noteName = this.settings.folderNoteName;
 		var folderNotePath = folderPath + '/' + noteName + '.md';
 		let hasFolderNote = await this.app.vault.adapter.exists(folderNotePath);
-		if (hasFolderNote) {
-			var linkPath = getLinkpath(folderNotePath);
-			// console.log('note link:', linkPath);
-			this.app.workspace.openLinkText(noteName, linkPath, false, { active: true });
-		} 
+		var showFolderNote = hasFolderNote;
+		if (!hasFolderNote && doCreate) {
+			const noteStrInit = this.settings.folderNoteStrInit;
+			await this.app.vault.adapter.write(folderNotePath, noteStrInit);
+			showFolderNote = true;
+		}
+
+		// show the note
+		if (showFolderNote) {
+			// modify the element
+			folderElem.addClass('has-folder-note');
+			folderElem.parentElement
+				.querySelectorAll('div.nav-folder-children > div.nav-file > div.nav-file-title')
+				.forEach(function (fileElem) {
+					// console.log('fileElem:', fileElem);
+					let fileDataPath = fileElem.attributes.getNamedItem('data-path').textContent;
+					if (fileDataPath.endsWith(folderNotePath)) {
+						fileElem.addClass('is-folder-note');
+					}
+			});
+
+			// show the note
+			this.app.workspace.openLinkText(folderNotePath, '', false, { active: true });
+		}
+	}
+}
+
+class FolderNoteSettingTab extends PluginSettingTab {
+	plugin: FolderNotePlugin;
+
+	constructor(app: App, plugin: FolderNotePlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
 	}
 
-	async createFoldNote(folderPath: string) {
-		const noteName = '_about_'
-		var folderNotePath = folderPath + '/' + noteName + '.md';
-		var linkPath = getLinkpath(folderNotePath);
-		let hasFolderNote = await this.app.vault.adapter.exists(folderNotePath);
-		if (!hasFolderNote) {
-			const noteStrInit = '# About the folder\n';
-			await this.app.vault.adapter.write(folderNotePath, noteStrInit);
-		}
-		this.app.workspace.openLinkText(noteName, linkPath, false, { active: true });
+	display(): void {
+		let {containerEl} = this;
+
+		containerEl.empty();
+
+		containerEl.createEl('h2', {text: 'Folder Note Plugin: Settings.'});
+
+		new Setting(containerEl)
+			.setName('Note Name')
+			.setDesc('Set the name for folder note. (Default: _about_) ')
+			.addText(text => text
+				.setValue(this.plugin.settings.folderNoteName)
+				.onChange(async (value) => {
+					// console.log('Secret: ' + value);
+					this.plugin.settings.folderNoteName = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Inital Content')
+			.setDesc('Set the inital content for new folder note.')
+			.addTextArea(text => {
+                    text
+                        .setPlaceholder('About the folder.')
+                        .setValue(this.plugin.settings.folderNoteStrInit)
+                        .onChange(async (value) => {
+                            try {
+                                const newValue = JSON.parse(value);
+                                this.plugin.settings.folderNoteStrInit = newValue;
+                                await this.plugin.saveSettings();
+                            } catch (e) {
+                                return false;
+                            }
+                        })
+                    text.inputEl.rows = 8;
+                    text.inputEl.cols = 50;
+                }
+            );
 	}
 }
